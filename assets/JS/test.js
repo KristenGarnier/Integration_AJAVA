9fa404e9fde099a744eafc0c214a17a63f4aefa3
KristenGarnier <garnier.kristen@icloud.com>
2015-06-15 23:21:28 +0200

    Finition de la page finale

--- assets/JS/test.js
+++ assets/JS/test.js
@@ -1,11 +1,37 @@
 $(document).ready(function () {
-    var height = $(window).height();
+
+    var height = windowSet();
+    if($('.inscription')){
+        inscriptionSet(height);
+    }
+
+    // quand on redimensionne la fenetre
+    $(window).resize(function(){
+        height = windowSet();
+        if($('.inscription')){
+            inscriptionSet(height);
+        }
+    })
+
+});
+
+// fonction pour centrer le formulaire d'inscription [non  terminée ]
+function inscriptionSet(height){
     var $inscription = $('.inscription');
-    $('body').height(height);
+
     var elementH = $inscription.height();
     var finalHeight = height/2 - elementH/2;
 
     $inscription.css({
         marginTop: finalHeight
     });
-});
+}
+
+
+// fonction pour que le fond occupe toute la page
+function windowSet(){
+    var height = $(window).height();
+    $('body').height(height);
+
+    return height;
+}
