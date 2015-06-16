a3235a7a7ce7769b4f480c6d4e8f488712a4f6a5
KristenGarnier <garnier.kristen@icloud.com>
2015-06-16 21:33:56 +0200

    Changement polices, quelques réglages dans gulp pour un le SCSS et le LIVE BROWSER, Intégration de l'image, quelques réglages stylistiques

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
-// récupérer la config gulp au boulot et la remetttre ici
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+
+gulp.task('sass', function () {
+    gulp.src('./assets/SCSS/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('default', ['watch'], function(){
+
+});
+
+gulp.task('watch', ['sass'], function() {
+
+    browserSync({
+        proxy: "localhost/intAJAVA/error.html"
+    });
+
+    gulp.watch("assets/SCSS/*.scss", ['sass']).on('change', reload);
+    gulp.watch("*.html").on('change', reload);
+});
