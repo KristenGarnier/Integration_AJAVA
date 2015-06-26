cef29dc98d5f2c5e0ba3df44cf5966871a9f23ea
KristenGarnier <garnier.kristen@icloud.com>
2015-06-26 20:04:05 +0200

    Html pour les trajets

---
+++ assets/JS/imageRemplacement.js
@@ -0,0 +1,18 @@
+$("#profile").change(function(){
+    readURL(this);
+});
+
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.imageSet')
+                .attr('src', e.target.result)
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
