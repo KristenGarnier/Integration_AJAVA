03551d5fcffa577895096b7f00560ff2a11e49c6
KristenGarnier <garnier.kristen@icloud.com>
2015-07-15 21:51:12 +0200

    Mise en place de la maps interactive

--- assets/JS/mapsAction.js
+++ assets/JS/mapsAction.js
@@ -1,14 +1,14 @@
-function next(origin, dest, lat, lng) {
+function next(lat, lng, origin, dest, etape) {
     var map = new GMaps({
         div: '#map-canvas',
         lat: 45.5667 || lat,
         lng: 5.9333 || lng,
-        zoom: 6
+        zoom: 7
     });
 
 
-    if (origin !== undefined && dest !== undefined) {
-       map.drawRoute({
+    if (origin !== undefined && dest !== undefined && etape === undefined) {
+        map.drawRoute({
             origin: origin,
             destination: dest,
             travelMode: 'driving',
@@ -17,25 +17,74 @@ function next(origin, dest, lat, lng) {
             strokeWeight: 6
         });
 
-        //Etape
-        map.drawRoute({
-            origin: origin,
-            destination: [45.40, 5.23],
-            travelMode: 'driving',
-            strokeColor: '#131540',
-            strokeOpacity: 0.6,
-            strokeWeight: 6
-        });
+    } else {
+        if (origin !== undefined && dest !== undefined && etape !== undefined) {
+            map.drawRoute({
+                origin: origin,
+                destination: etape,
+                travelMode: 'driving',
+                strokeColor: '#131540',
+                strokeOpacity: 0.6,
+                strokeWeight: 6
+            });
+
+            map.drawRoute({
+                origin: etape,
+                destination: dest,
+                travelMode: 'driving',
+                strokeColor: '#131540',
+                strokeOpacity: 0.6,
+                strokeWeight: 6
+            });
+        }
+
     }
 }
 
 next();
 
 
-$('.weather').click(function () {
-    console.log('click');
-    origin = [45.50, 5.59];
-    dest = [45.55, 5.9333];
-    next(origin, dest, origin[0], origin[1]);
+
+var origin;
+var dest;
+var etape;
+
+
+$('#depart').change(function () {
+    console.log($(this));
+    origin = [$(this).find(':selected').data('lat'), $(this).find(':selected').data('long')];
+    testNewCard();
 });
 
+$('#arrivee').change(function () {
+    dest = [$(this).find(':selected').data('lat'), $(this).find(':selected').data('long')];
+    testNewCard();
+});
+$('#etape').change(function () {
+    etape = [$(this).find(':selected').data('lat'), $(this).find(':selected').data('long')];
+    testNewCard();
+});
+
+function testNewCard() {
+    console.log(origin, dest, etape);
+    if (origin !== undefined && dest !== undefined && etape === undefined) {
+        console.log('1');
+        next(origin[0], origin[1], origin, dest);
+    }else {
+        if (origin !== undefined && dest !== undefined && etape !== undefined) {
+            console.log('2');
+            next(origin[0], origin[1], origin, dest, etape);
+        }
+    }
+}
+
+// On récupère les valeurs dans les champs,
+// Si au moins point de départ et de destination son renseignés on affiche la route,
+// on prend pour origine de la carte le point de départ
+// Si une étape est définie, on réaffiche la carte
+
+// une fois que le point d'arrivée est définit, on va chercher la météo dans le weather
+
+// COORD LYON [45.75,4.85]
+// COORD GRENOBLE [45.188529, 5.724523999999974]
+// COORD CHAMBERY [45.564601, 5.917780999999991]
